44f848d93a9fd43bb6231d1c8f7424beb5c9992f
EPPP <jayepep@163.com>
2021-12-24 16:24:03 +0800

    use progress sign

--- main_vdsr.py
+++ main_vdsr.py
@@ -6,32 +6,34 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from vdsr import Net
+from vdsr import Net, BConv2d
 from dataset import DatasetFromHdf5
+import time
 
-# Training settings
 parser = argparse.ArgumentParser(description="PyTorch VDSR")
 parser.add_argument("--batchSize", type=int, default=128, help="Training batch size")
 parser.add_argument("--nEpochs", type=int, default=50, help="Number of epochs to train for")
 parser.add_argument("--lr", type=float, default=0.1, help="Learning Rate. Default=0.1")
 parser.add_argument("--step", type=int, default=10, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=10")
-parser.add_argument("--cuda", action="store_true", help="Use cuda?")
-parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (default: none)")
+parser.add_argument("--cuda", default=True, help="Use cuda?")
+parser.add_argument("--parallel", default=0, type=int, help = "number of GPUs for parallel training")
+parser.add_argument("--resume", default="none", type=str, help="Path to checkpoi    nt (default: none)")
 parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
 parser.add_argument("--clip", type=float, default=0.4, help="Clipping Gradients. Default=0.4")
-parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, Default: 1")
+parser.add_argument("--threads", type=int, default=1, help="Number of threads for data loader to use, Default: 1")
 parser.add_argument("--momentum", default=0.9, type=float, help="Momentum, Default: 0.9")
 parser.add_argument("--weight-decay", "--wd", default=1e-4, type=float, help="Weight decay, Default: 1e-4")
-parser.add_argument('--pretrained', default='', type=str, help='path to pretrained model (default: none)')
+parser.add_argument('--pretrained', default='none', type=str, help='path to pretrained model (default: none)')
 parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+parser.add_argument('--local_rank', default=-1, type=int, help='node rank for distributed training')
 
 def main():
     global opt, model
     opt = parser.parse_args()
     print(opt)
-
     cuda = opt.cuda
     if cuda:
+        print("cuda")
         print("=> use gpu id: '{}'".format(opt.gpus))
         os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpus
         if not torch.cuda.is_available():
@@ -39,26 +41,27 @@ def main():
 
     opt.seed = random.randint(1, 10000)
     print("Random Seed: ", opt.seed)
+    # Sets the seed for generating random numbers.
     torch.manual_seed(opt.seed)
     if cuda:
         torch.cuda.manual_seed(opt.seed)
 
     cudnn.benchmark = True
-
     print("===> Loading datasets")
     train_set = DatasetFromHdf5("data/train.h5")
-    training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
+    training_data_loader = DataLoader(dataset=train_set, batch_size=opt.batchSize, shuffle=True)
 
     print("===> Building model")
     model = Net()
-    criterion = nn.MSELoss(size_average=False)
+
+    criterion = nn.MSELoss(reduction='sum')
 
     print("===> Setting GPU")
     if cuda:
         model = model.cuda()
         criterion = criterion.cuda()
 
-    # optionally resume from a checkpoint
+    # optionally resume from a checkpoint  
     if opt.resume:
         if os.path.isfile(opt.resume):
             print("=> loading checkpoint '{}'".format(opt.resume))
@@ -67,7 +70,8 @@ def main():
             model.load_state_dict(checkpoint["model"].state_dict())
         else:
             print("=> no checkpoint found at '{}'".format(opt.resume))
-
+    else:
+        print("opt.resume == none")
     # optionally copy weights from a checkpoint
     if opt.pretrained:
         if os.path.isfile(opt.pretrained):
@@ -75,46 +79,63 @@ def main():
             weights = torch.load(opt.pretrained)
             model.load_state_dict(weights['model'].state_dict())
         else:
-            print("=> no model found at '{}'".format(opt.pretrained))  
+            print("=> no model found at '{}'".format(opt.pretrained))
 
     print("===> Setting Optimizer")
     optimizer = optim.SGD(model.parameters(), lr=opt.lr, momentum=opt.momentum, weight_decay=opt.weight_decay)
-
+    """
+    Batch size is set to 16. Adam is utilized to optimize the network. The momentum parameter is set to 0.5, 
+    weight decay is set to 2 × 10−4, and the initial learning rate is set to 1 × 10−4 and will be divided 
+    a half every 200 epochs.
+    """
+    # optimizer = optim.Adam(model.parameters(), lr=1e-3)
     print("===> Training")
     for epoch in range(opt.start_epoch, opt.nEpochs + 1):
-        train(training_data_loader, optimizer, model, criterion, epoch)
+        temperature = set_temperature(model, epoch)
+        train(training_data_loader, optimizer, model, criterion, epoch, temperature)
+        # writer.close()
         save_checkpoint(model, epoch)
 
 def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 10 epochs"""
     lr = opt.lr * (0.1 ** (epoch // opt.step))
     return lr
 
-def train(training_data_loader, optimizer, model, criterion, epoch):
-    lr = adjust_learning_rate(optimizer, epoch-1)
+def set_temperature(model, epochs):
+    Temperature = torch.ones(1)
+    for i, module in enumerate(model.modules()):
+        if isinstance(module, BConv2d):
+            if epochs != 0:
+                module.update_temperature()
+            Temperature = torch.Tensor([module.temperature])
+    return Temperature
 
+def train(training_data_loader, optimizer, model, criterion, epoch, temperature):
+    lr = adjust_learning_rate(optimizer, epoch-1)
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
-
     print("Epoch = {}, lr = {}".format(epoch, optimizer.param_groups[0]["lr"]))
-
     model.train()
-
     for iteration, batch in enumerate(training_data_loader, 1):
         input, target = Variable(batch[0]), Variable(batch[1], requires_grad=False)
 
         if opt.cuda:
             input = input.cuda()
             target = target.cuda()
-
         loss = criterion(model(input), target)
+
+        if iteration % 100 == 0:
+            output = "===> Epoch[{}]({}/{}): Loss: {:.10f} temperature: {:.3f} learning rate: {:.10f}".format(epoch, iteration, len(training_data_loader), loss.item(), temperature.item(), opt.lr)
+            with open("loss.txt", "a+") as f:
+                f.write(output + '\n')
+                f.close
+
         optimizer.zero_grad()
-        loss.backward() 
-        nn.utils.clip_grad_norm(model.parameters(),opt.clip) 
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), opt.clip)
         optimizer.step()
 
         if iteration%100 == 0:
-            print("===> Epoch[{}]({}/{}): Loss: {:.10f}".format(epoch, iteration, len(training_data_loader), loss.item()))
+          print("===> Epoch[{}]({}/{}): Loss: {:.10f} temperature: {:.3f}".format(epoch, iteration, len(training_data_loader), loss.item(), temperature.item()))
 
 def save_checkpoint(model, epoch):
     model_out_path = "checkpoint/" + "model_epoch_{}.pth".format(epoch)
@@ -128,3 +149,4 @@ def save_checkpoint(model, epoch):
 
 if __name__ == "__main__":
     main()
+
--- vdsr.py
+++ vdsr.py
@@ -3,26 +3,31 @@ import torch.nn as nn
 from math import sqrt
 import torch.nn.functional as F
 
-def binarize(x, scale = True):
+def binarize(x, temperature=1.0, progressive=True, scale=False):
     replace = x.clamp(-1, 1)
-    mean = replace.abs().mean(-1, keepdim = True).mean(-2, keepdim = True).mean(-3, keepdim = True)
+    if scale:
+        mean = abs(x).mean(-1, keepdim=True).mean(-2, keepdim=True).mean(-3, keepdim=True)
+    else:
+        mean = 1.0
+    with torch.no_grad():
+        binary = F.hardtanh(x/max(temperature, 1e-8))
+        if not progressive:
+            binary = binary.sign()
+    return ((binary - replace).detach() + replace).mul(mean)
 
-    return ((torch.sign(x) - replace).detach() + replace).mul(mean)
-
-
-class BConv(nn.Module):
-    def __init__(self, 
-            in_channels = 64,
-            out_channels = 64,
-            kernel_size = 3,
-            stride = 1,
-            padding = 1,
-            dilation = 1,
-            groups = 1,
-            bias = False
-            ):
-        super(BConv, self).__init__()
-
+class BConv2d(nn.Module):
+    def __init__(self,
+                 in_channels=64,
+                 out_channels=64,
+                 kernel_size=3,
+                 stride=1,
+                 padding=1,
+                 dilation=1,
+                 groups=1,
+                 bias=False,
+                 wb=True,
+                 ab=True):
+        super(BConv2d, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
@@ -30,47 +35,52 @@ class BConv(nn.Module):
         self.padding = padding
         self.dilation = dilation
         self.groups = groups
-        
-        assert in_channels % self.groups == 0, "The number of input channels can not be splited uniformly by number {}".format(self.groups)
-
-        self.weight = nn.Parameter(torch.randn(out_channels, int(in_channels//groups), kernel_size, kernel_size)*1e-2)
-        self.bias = nn.Parameter(torch.zeros(1, out_channels, 1, 1)) if bias else None
-        
+        self.wb = wb
+        self.ab = ab
+        self.register_buffer("temperature", torch.Tensor([1]))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_channels))
+        else:
+            self.bias = None
+        self.weight = nn.Parameter(torch.randn(out_channels, int(in_channels // groups), kernel_size, kernel_size))
         self.weight_init()
-
+        if self.padding > 0:
+            self.replicatepad = nn.ReplicationPad2d(padding=(padding, padding, padding, padding))
+        else:
+            self.replicatepad = lambda x: x
     def weight_init(self):
-        nn.init.kaiming_normal_(self.weight, mode = 'fan_out', nonlinearity = 'relu')
-        
+        nn.init.kaiming_normal_(self.weight, mode='fan_out', nonlinearity='relu')
+
+    def update_temperature(self):
+            self.temperature *= 0.965
+
     def forward(self, x):
-        weight = binarize(self.weight)
+        if self.wb:
+            weight = binarize(self.weight, self.temperature, progressive=self.training, scale=False)
+        x = x.view(x.size(0), -1, x.size(-2), x.size(-1))
+        x = self.replicatepad(x)    # 对外围填充
+        out = F.conv2d(input=x,
+                       weight=weight,
+                       bias=self.bias,
+                       stride=self.stride,
+                       padding=0,
+                       dilation=self.dilation,
+                       groups=self.groups)
+        return out
 
-        return F.conv2d(
-                    x,
-                    weight,
-                    bias = self.bias,
-                    padding = self.padding,
-                    dilation = self.dilation,
-                    groups = self.groups
-                )
 
-class Conv_ReLU_Block(nn.Module):
-    def __init__(self):
-        super(Conv_ReLU_Block, self).__init__()
-        self.conv = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        
-    def forward(self, x):
-        return self.relu(self.conv(x))
-
+# 神经网络结构块
 class BConv_ReLU_Block(nn.Module):
     def __init__(self):
         super(BConv_ReLU_Block, self).__init__()
-        self.conv = BConv(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding =1, bias = False)
+        self.conv = BConv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         return self.relu(self.conv(x))
 
+
+# 主要网络结构
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -78,12 +88,12 @@ class Net(nn.Module):
         self.input = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.output = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
-    
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, sqrt(2. / n))
-                
+
     def make_layer(self, block, num_of_layer):
         layers = []
         for _ in range(num_of_layer):
@@ -95,6 +105,5 @@ class Net(nn.Module):
         out = self.relu(self.input(x))
         out = self.residual_layer(out)
         out = self.output(out)
-        out = torch.add(out,residual)
+        out = torch.add(out, residual)
         return out
- 
